52580858473341bbf3adb4226513679f42dc9f1d
songzhenzhe <songzhenzhe@gomeplus.com>
2017-12-19 12:13:09 +0800

    fix

--- ActualCombat/File/mcb.py
+++ ActualCombat/File/mcb.py
@@ -12,14 +12,20 @@ mcbShelf = shelve.open("mcb")
 
 if len(sys.argv) == 3 and sys.argv[1].lower() == "save":
     mcbShelf[sys.argv[2]] = pyperclip.paste()
-    print("save ok", mcbShelf[sys.argv[2]])
-    print(list(mcbShelf.keys()))  # ['pam', 'sapm']
-    print(mcbShelf['pam'])
-    print(mcbShelf['spam'])
 elif len(sys.argv) == 2:
     if sys.argv[1].lower() == "list":
-        print(pyperclip.copy(str(list(mcbShelf.keys()))))
+        pyperclip.copy(str(list(mcbShelf.keys())))
+        print(pyperclip.paste())
     elif sys.argv[1] in mcbShelf:
         pyperclip.copy(mcbShelf[sys.argv[1]])
+        print(pyperclip.paste())
+
+if len(sys.argv) == 3 and sys.argv[1].lower() == "delete":
+    if sys.argv[2] in mcbShelf:
+        del mcbShelf[sys.argv[2]]
+elif len(sys.argv) == 2:
+    if sys.argv[1].lower() == 'delete':
+        for i in mcbShelf:
+            del mcbShelf[i]
 
 mcbShelf.close()
